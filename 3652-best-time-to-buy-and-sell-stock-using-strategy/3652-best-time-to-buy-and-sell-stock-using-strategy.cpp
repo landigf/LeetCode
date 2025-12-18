9c672305175dd119b9997623c753efc780ac4978
landigf <95249781+landigf@users.noreply.github.com>
2025-12-18 08:18:27 +0100

    Time: 2310 ms (5.48%), Space: 328.2 MB (22.98%) - LeetHub

---
+++ 3652-best-time-to-buy-and-sell-stock-using-strategy/3652-best-time-to-buy-and-sell-stock-using-strategy.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    long long maxProfit(vector<int>& prices, vector<int>& strategy, int k) {
+        int n = prices.size();
+        vector<long long> original(n+1), full(n+1);
+        for(int i = 1; i <= n; ++i){
+            original[i] = prices[i-1]*strategy[i-1];
+            original[i] += original[i-1];
+            full[i] = prices[i-1] + full[i-1];
+        }
+        int l = 1, r = k;
+        long long res = original[n];
+        while(r <= n){
+            int i = l + (r-l)/2;
+            long long curr = 0;
+            curr += original[l-1] - original[0];
+            cout << curr<<", "; 
+            curr += original[n] - original[r];
+            cout << curr<<", "; 
+            curr += full[r] - full[i];
+            res = max(res, curr);
+            ++l; ++r;
+        }
+        return res;
+    }
+};
