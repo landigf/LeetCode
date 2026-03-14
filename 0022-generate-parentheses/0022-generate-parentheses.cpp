c1e3bb3728d321c396f36499dc4ae3c8e65f35d7
landigf <95249781+landigf@users.noreply.github.com>
2026-03-14 19:33:11 +0100

    Time: 1 ms (85.62%), Space: 12.9 MB (88.97%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> res;
+        string cur;
+        backtrack(n, 0, 0, cur, res);
+        return res;
+    }
+
+    void backtrack(int n, int open, int close, string& cur, vector<string>& res) {
+        if (cur.size() == 2 * n) {
+            res.push_back(cur);
+            return;
+        }
+
+        if (open < n) {
+            cur.push_back('(');
+            backtrack(n, open + 1, close, cur, res);
+            cur.pop_back();
+        }
+
+        if (close < open) {
+            cur.push_back(')');
+            backtrack(n, open, close + 1, cur, res);
+            cur.pop_back();
+        }
+    }
+};
